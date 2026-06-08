dc79513d19bdb80b1c35f3f2f40f82c12abb9a4a
Vedant Ladda <ladda.v@northeastern.edu>
2026-06-08 09:02:18 +0530

    Revert Render URL overrides breaking websockets

--- rxconfig.py
+++ rxconfig.py
@@ -11,17 +11,7 @@ _api_url = os.environ.get("API_URL")
 _deploy_url = os.environ.get("DEPLOY_URL")
 _cors = os.environ.get("CORS_ORIGINS")
 
-if os.environ.get("RENDER"):
-    render_url = os.environ.get("RENDER_EXTERNAL_URL")
-    if render_url:
-        _api_url = _api_url or render_url
-        _deploy_url = _deploy_url or render_url
 
-if _api_url and _api_url.startswith("http://") and "onrender.com" in _api_url:
-    _api_url = _api_url.replace("http://", "https://")
-
-if _deploy_url and _deploy_url.startswith("http://") and "onrender.com" in _deploy_url:
-    _deploy_url = _deploy_url.replace("http://", "https://")
 
 _kwargs = dict(
     app_name="fantasy_auction",
